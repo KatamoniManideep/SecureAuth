544f380d2be2b5bed8e17bb8a8df33eb9b75e673
Katamoni Manideep <katamonimanideep04@gmail.com>
2026-02-24 22:38:36 +0530

    first commit

---
+++ src/modules/auth/auth.types.ts
@@ -0,0 +1,13 @@
+
+export interface User{
+    id: string,
+    email: string,
+    password: string,
+    role: 'user' | 'admin',
+    created_at: Date
+}
+
+export interface JwtPayload {
+    userId: string,
+    role: 'role' | 'admin'
+}
